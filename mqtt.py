729b32d39d111a44f9ed8c5afac98b27c48a8bcb
davidcheng220 <65834166+davidcheng220@users.noreply.github.com>
2025-01-22 15:57:09 +0800

    Add files via upload

---
+++ mqtt.py
@@ -0,0 +1,21 @@
+import paho.mqtt.client as mqtt  
+import json
+
+MQTT_SERVER = "0.0.0.0"  
+MQTT_PORT = 1883  
+MQTT_ALIVE = 60  
+MQTT_TOPIC = "msg/info"
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    client.subscribe(MQTT_TOPIC)
+
+def on_message(client, userdata, msg):
+    # print(f"{msg.topic}: {msg.payload.decode('utf-8')}"
+    print(f"{msg.topic} - data1: {json.loads(msg.payload)}")
+
+mqtt_client = mqtt.Client()
+mqtt_client.on_connect = on_connect
+mqtt_client.on_message = on_message
+mqtt_client.connect(MQTT_SERVER, MQTT_PORT, MQTT_ALIVE)  
+mqtt_client.loop_forever()
